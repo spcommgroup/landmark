6bb3cd573c1235a3517f83db9a712e4e0b6ae305
Jessica Kenney <seveneightn9ne@gmail.com>
2013-06-17 15:50:04 -0400

    Updated LexiconExtract to ask user input and save as CMU-style dictionary

--- LexiconExtract.py
+++ LexiconExtract.py
@@ -2,49 +2,68 @@ import pickle
 from TGProcess import *
 
 # Dictionary Path
-DICT=open("cmudict.0.7a")
+cmupath = "cmudict.0.7a"
+DICT=open(cmupath)
 
 # File Path(s)
-wordfiles = ["conv01_landmarks_ssh-edits_3-11-09_no-auto-labels.textgrid",
-             "conv02g_ym_fixed_lma_ssh_2-25-08-to-nmv.textgrid",
-             "conv03g-or-5__ssh_9-11-08.textgrid",
-             "conv04g_fixed_lma_7-24-07_ssh-3-28-08.textgrid",
-             "conv05g_merged_1-complete.textgrid",
-             "conv06g_ch_fixed.textgrid",
-             "conv08gdmw.textgrid",
-             "yinmon07g_lm.textgrid"
-             ]
-## TO-TO: ADD PROMPT INTERFACE
+# --using user prompt instead--
+# wordfiles = ["conv01_landmarks_ssh-edits_3-11-09_no-auto-labels.textgrid",
+#              "conv02g_ym_fixed_lma_ssh_2-25-08-to-nmv.textgrid",
+#              "conv03g-or-5__ssh_9-11-08.textgrid",
+#              "conv04g_fixed_lma_7-24-07_ssh-3-28-08.textgrid",
+#              "conv05g_merged_1-complete.textgrid",
+#              "conv06g_ch_fixed.textgrid",
+#              "conv08gdmw.textgrid",
+#              "yinmon07g_lm.textgrid"
+#              ]
+
+f = False
+while not f:
+    path = input(".textgrid file path: ")
+    if path.lower().endswith(".textgrid"):
+        try:
+            f = open(path, "r")
+        except IOError:
+            print ("The file does not exist")
+    else:
+        print ("Must be a .textgrid file")
 
 lexicon = {}
 vocab = []
 
-for fname in wordfiles:
-    print ("\nprocessing file", fname, " ...")
-    wordfile=open(fname)
-    words = {}
-    count=0
-    
-    # Find word tier
-    tg = TextGrid()
-    tg.readGridFromPath(fname)
-    for tier in tg:
-        if 'word' in tier.name.lower() or 'conv' in tier.name.lower():     # Word tier is named differently in different textgrid files
-            text = tier
-            break
-    # OR, assuming that text tier always comes first:
-##    text = tg.tiers[0]    
-    print ("found text tier ", text)
-    
-    for interval in text:
-        word = interval.text.strip(" ?.\t\"").lower()
-        if word in words:
-            words[word]+=1
-        else:
-            words[word]=1
+# wordfile=open(fname)
+words = {}
+count=0
+text = ""
 
-    vocab += words.keys()
-    wordfile.close()
+# Find word tier
+tg = TextGrid()
+tg.readGridFromPath(path)
+for tier in tg:
+    if 'word' in tier.name.lower() or 'conv' in tier.name.lower():     # Word tier is named differently in different textgrid files
+        text = tier
+        break
+#OR, assuming that text tier always comes first:
+if not text:
+    text = tg.tiers[0]    
+print ("found text tier ", text)
+use = input("Use this tier (yes/no)? ")
+if not (use.lower()=="yes" or use.lower()=="y"):
+    for tier, i in zip(tg, range(len(tg.tiers))):
+        print (i + " " + tier.name)
+    use = input("Use tier: ")
+    text = tg.tiers[int(use)]
+
+print ("\nprocessing file", path, " ...")
+
+for interval in text:
+    word = interval.text.strip(" ?.\t\"").lower()
+    if word in words:
+        words[word]+=1
+    else:
+        words[word]=1
+
+vocab += words.keys()
 
 vocab = list(set(vocab))
 anomalies = vocab           # irregular pronounciation
@@ -56,7 +75,7 @@ for entry in DICT:
             lexicon[word] = entry.strip('\n').lower()
             anomalies.remove(word)
 
-
+#print (lexicon)
 # Output File
 out=open("lexicon", 'wb')
 pickle.dump(lexicon, out)
@@ -64,3 +83,29 @@ pickle.dump(lexicon, out)
 out.close()
 DICT.close()
 
+print("Warning! The following words were not found in the dictionary:")
+for word in anomalies:
+    if not word.startswith("<") and not word.endswith(">"):
+        print("\t"+word)
+fix = input("Fix anomalies now (yes/no)? ")
+if fix.lower()=="yes":
+    for word in anomalies:
+        pron = input(word+": ")
+        if pron:
+            lexicon[word]=pron
+
+# jessk added 6/17/13: Save as CMU-formatted Dictionary
+if path.lower().endswith(".textgrid"):
+    outpath = path[:-9]
+else:
+    outpath = path
+outpath += "_lexicon.txt"
+
+out = open(outpath, "w")
+out.write(";;; This lexicon is a subset of CMUdict containing words from "+path+"\n")
+out.write(";;; It was generated from the CMU dictionary file "+cmupath+"\n")
+for word in lexicon:
+    out.write(lexicon[word]+"\n")
+
+print("Saved to "+outpath+".")
+out.close()
