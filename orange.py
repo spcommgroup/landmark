5da4389622e474c3c1ebddf4ffa8376ddb662781
Jessica Kenney <seveneightn9ne@gmail.com>
2013-08-22 15:21:36 -0400

    Added 2nd level trees with phone2-manner class

--- orange.py
+++ orange.py
@@ -14,11 +14,21 @@ tree = Orange.classification.tree.TreeLearner(data)
 # print tree.to_string()
 tree.dot(file_name=os.path.join("results","conv07 trees","full_tree.dot"), node_shape="ellipse", leaf_shape="box")
 
+attributes_names = ["phone2-manner class",]
+attributes = []
+for attribute_name in attributes_names:
+	attributes.extend([x for x in data.domain.features if x.name == attribute_name])
+outpath = os.path.join("results","conv07 trees",*attributes_names)
+call(["mkdir", "-p", outpath])
+print("attributes =", [x.name for x in attributes])
+
 for feature_id in range(len(data.domain[:len(data.domain)-1])):
-	new_domain = Orange.data.Domain(list(data.domain)[feature_id], data.domain.class_var)
-	new_data = Orange.data.Table(new_domain, data)
-	new_tree = Orange.classification.tree.TreeLearner(new_data)
-	file_name = os.path.join("results","conv07 trees",list(data.domain)[feature_id].name+".dot")
-	new_tree.dot(file_name=file_name, node_shape="ellipse", leaf_shape="box")
-	# os.system("dot -Tpng '"+file_name+"' -o '"+file_name[:-3]+"png'")
-	call(["dot","-Tpng",file_name,"-o",file_name[:-3]+"png"])
+	if not (data.domain[feature_id] in attributes):
+		new_domain = Orange.data.Domain(attributes + [list(data.domain)[feature_id]], data.domain.class_var)
+		new_data = Orange.data.Table(new_domain, data)
+		new_tree = Orange.classification.tree.TreeLearner(new_data)
+
+		file_name = os.path.join(outpath, list(data.domain)[feature_id].name+".dot")
+		new_tree.dot(file_name=file_name, node_shape="ellipse", leaf_shape="box")
+		# os.system("dot -Tpng '"+file_name+"' -o '"+file_name[:-3]+"png'")
+		call(["dot","-Tpng",file_name,"-o",file_name[:-3]+"png"])
