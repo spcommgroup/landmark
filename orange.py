83a0787839d8ac8c35a61c21a636e999f263460b
Jess Kenney <seveneightn9ne@gmail.com>
2013-09-10 19:44:06 -0400

    Add phone2 tree

--- orange.py
+++ orange.py
@@ -5,30 +5,41 @@ import os
 from subprocess import call
 
 data = Orange.data.Table("data/source/Conv07_choi_20130228_final.tab")
-
-# print "Attributes:", ", ".join(x.name for x in data.domain.features)
-# print "Class:", data.domain.class_var.name
-# print "Data instances", len(data)
-
 tree = Orange.classification.tree.TreeLearner(data)
-# print tree.to_string()
-tree.dot(file_name=os.path.join("results","conv07 trees","full_tree.dot"), node_shape="ellipse", leaf_shape="box")
 
-attributes_names = ["phone2-manner class",]
+all_attributes = ["phone1-manner class", "phone2-type", "phone2-subnumber", 
+                  "phone2-manner class", "outcome", "name", "phone1-subnumber",
+                  "phone2-number", "phone2-stress", "phone1-stress",
+                  "phone1-type", "phone1-number"]
+################
+#### Config ####
+################
+attributes_names = filter(lambda x: x.startswith("phone2"), all_attributes) + ["outcome"]
+save_path = os.path.join("results", "conv07 trees", "phone2")
+tree_file_name = "phone2-all.dot" #will also save a .png with the same name
+####################
+#### End Config ####
+####################
+
+# print tree.to_string()
+# tree.dot(file_name=os.path.join("results","conv07 trees","full_tree.dot"), node_shape="ellipse", leaf_shape="box")
+
 attributes = []
 for attribute_name in attributes_names:
-	attributes.extend([x for x in data.domain.features if x.name == attribute_name])
-outpath = os.path.join("results","conv07 trees",*attributes_names)
+    attributes.extend([x for x in data.domain.features if x.name == attribute_name])
+#outpath = os.path.join("results","conv07 trees",*attributes_names)
+outpath = save_path
 call(["mkdir", "-p", outpath])
 print("attributes =", [x.name for x in attributes])
 
 for feature_id in range(len(data.domain[:len(data.domain)-1])):
-	if not (data.domain[feature_id] in attributes):
-		new_domain = Orange.data.Domain(attributes + [list(data.domain)[feature_id]], data.domain.class_var)
-		new_data = Orange.data.Table(new_domain, data)
-		new_tree = Orange.classification.tree.TreeLearner(new_data)
+    if not (data.domain[feature_id] in attributes):
+        new_domain = Orange.data.Domain(attributes + [list(data.domain)[feature_id]], data.domain.class_var)
+        new_data = Orange.data.Table(new_domain, data)
+        new_tree = Orange.classification.tree.TreeLearner(new_data)
 
-		file_name = os.path.join(outpath, list(data.domain)[feature_id].name+".dot")
-		new_tree.dot(file_name=file_name, node_shape="ellipse", leaf_shape="box")
-		# os.system("dot -Tpng '"+file_name+"' -o '"+file_name[:-3]+"png'")
-		call(["dot","-Tpng",file_name,"-o",file_name[:-3]+"png"])
+        # file_name = os.path.join(outpath, list(data.domain)[feature_id].name+".dot")
+        file_name = os.path.join(outpath, tree_file_name)
+        new_tree.dot(file_name=file_name, node_shape="ellipse", leaf_shape="box")
+        # os.system("dot -Tpng '"+file_name+"' -o '"+file_name[:-3]+"png'")
+        call(["dot","-Tpng",file_name,"-o",file_name[:-3]+"png"])
